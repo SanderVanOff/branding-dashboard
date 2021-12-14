e4d4ae763ce927182a8510076384fdfc3a8d7d84
SanderVanOff <7okay@mail.ru>
2021-12-14 21:47:14 +0500

    api key в скрытые файлы

--- src/main.js
+++ src/main.js
@@ -28,14 +28,14 @@ Vue.config.productionTip = false;
 
 Vue.use(VueEasytable);
 initializeApp({
-  apiKey: "AIzaSyDSMCuARRsfagl5_QaKCEvpDAW-BHJ4vHY",
-  authDomain: "ads-on-cars-e195e.firebaseapp.com",
-  databaseURL: "https://ads-on-cars-e195e-default-rtdb.firebaseio.com",
-  projectId: "ads-on-cars-e195e",
-  storageBucket: "ads-on-cars-e195e.appspot.com",
-  messagingSenderId: "797844828666",
-  appId: "1:797844828666:web:2f366fa23ff35d5f49f443",
-  measurementId: "G-3XH1JWYT8N"
+  apiKey: `${process.env.VUE_APP_API_KEY}`,
+  authDomain: `${process.env.VUE_APP_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.VUE_APP_DATABASE_URL}`,
+  projectId: `${process.env.VUE_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.VUE_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.VUE_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.VUE_APP_ID}`,
+  measurementId: `${process.env.VUE_APP_MEASUREMENT_ID}`
 });
 
 const auth = getAuth();
